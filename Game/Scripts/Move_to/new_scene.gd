8ceec3d2e1a1c3e4ab80c4b350ce43e3b6f96e09
SytnikovaAlina <rostovchanka180@gmail.com>
2024-05-23 22:20:24 +0300

    й

--- Game/Scripts/Move_to/new_scene.gd
+++ Game/Scripts/Move_to/new_scene.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var Security = preload("res://Scenes/security_zone.tscn") 
+var Security = preload("res://scenes/Rsue/security_zone.tscn") 
 
 func _on_body_entered(body):
 	get_tree().change_scene_to_packed(Security)
