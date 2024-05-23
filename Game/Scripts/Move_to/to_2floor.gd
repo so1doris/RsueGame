f220b75c8333e72c3e9b18a57b474455beca2976
soldoris <pavel.astap3nko@gmail.com>
2024-05-23 21:48:04 +0300

    1

--- Game/Scripts/Move_to/to_2floor.gd
+++ Game/Scripts/Move_to/to_2floor.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var second_floor = preload("res://Scenes/second_floor.tscn") 
+var second_floor = preload("res://scenes/Rsue/second_floor.tscn") 
 
 func _on_body_entered(body):
 	get_tree().change_scene_to_packed(second_floor)
--- Game/Scripts/Move_to/to_tk.gd
+++ Game/Scripts/Move_to/to_tk.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var Tk = preload("res://Scenes/tk.tscn")
+var Tk = preload("res://scenes/Rsue/tk.tscn")
  
 func _on_body_entered(body):
 	get_tree().change_scene_to_packed(Tk)
