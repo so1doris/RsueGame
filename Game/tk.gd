8cc0ac7e1a60561bb97f80f480a653fb32996625
SytnikovaAlina <rostovchanka180@gmail.com>
2024-05-24 08:34:47 +0300

    /

--- Game/tk.gd
+++ Game/tk.gd
@@ -1,9 +1,10 @@
 extends Node
 
-
+@export var Start_Scen = preload("res://MiniQuiz/Game/game.tscn")
 @onready var dialogue_box = $DialogueBox
 
 
 func _ready():
 	dialogue_box.start('tk_start')
+
 
